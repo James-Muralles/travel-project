1378251e5190c2a7eee77e072f4b1d0bc79ef71d
James-Muralles <james.muralles23@gmail.com>
2020-10-07 09:55:10 -0500

    added update functionallity

--- src/api/logs.js
+++ src/api/logs.js
@@ -1,10 +1,8 @@
-const { Router } = require('express');
+const { Router } = require("express");
 // import { Router } from "express";
-const LogEntry = require('../models/LogEntry');
+const LogEntry = require("../models/LogEntry");
 // import LogEntry from "../models/LogEntry";
 const router = Router();
-
-
 
 // router.get("/", (req, res) => {
 // 	res.json({
@@ -12,28 +10,53 @@ const router = Router();
 // 	});
 // });
 
-router.get("/",async (req, res, next) => {
-    try{   
-    const entries = await LogEntry.find();
-        res.json(entries);
-    }catch(error){
-        next(error)
-    }
+router.get("/", async (req, res, next) => {
+	try {
+		const entries = await LogEntry.find();
+		res.json(entries);
+	} catch (error) {
+		next(error);
+	}
 });
-
 
 router.post("/", async (req, res, next) => {
 	try {
-        const logEntry = new LogEntry(req.body);
+		const logEntry = new LogEntry(req.body);
 		const createdEntry = await logEntry.save();
-        console.log(createdEntry);
-        res.json(createdEntry);
+		console.log(createdEntry);
+		res.json(createdEntry);
 	} catch (error) {
-        if (error.name === "ValidationError") {
-            res.status(422);
+		if (error.name === "ValidationError") {
+			res.status(422);
 		}
 		next(error);
 	}
+});
+
+router.put("/update/:id", async (req, res) => {
+	let id = req.params.id;
+	try {
+		LogEntry.findById(id).then((logEntry) => {
+			console.log(logEntry);
+			if (!logEntry) {
+				res.status(404).send("data not found");
+			} else {
+                logEntry.title       = req.body.title;
+                logEntry.comments    = req.body.comments;
+                logEntry.description = req.body.description;
+                logEntry.visitDate   = req.body.visitDate;
+                logEntry.latitude    = req.body.latitude;
+                logEntry.longitude   = req.body.longitude;
+                logEntry.rating      = req.body.rating;
+                logEntry.image       = req.body.image;
+                logEntry.save();
+                res.json(logEntry)
+                console.log(logEntry)
+			}
+		});
+	} catch (error) {
+        res.status(400)
+    }
 });
 
 export default router;
